bdfb329faac23054668b22aa4df6cd3fb6ef4a04
Heiko Heiko <heiko@heiko.org>
2012-11-19 10:23:04 +0100

    use generalized fetcher

--- fetcharticles.py
+++ fetcharticles.py
@@ -1,45 +1,22 @@
 #!/usr/bin/env python
-'''
-
-
-'''
 import sys, os
-import urllib2
-import time
+import layout
+import fetcher
 
-
-user_agent = 'WP1.0 Bot - fetching 100K articles from cache / report problems to pediapress '
-user_agent = 'Python-urllib/2.6'
-fetch_delay = 0.
-
-def safe_fn(fn):
-    return fn.replace('/', '%2F')
-
-def fetch(name):
-    url = 'http://en.m.wikipedia.org/wiki/' + urllib2.quote(name.encode('utf8'), safe='')
-    req = urllib2.Request(url, headers={'User-Agent':user_agent})
-    res = urllib2.urlopen(req)
-    return res.read()
+def report_cb(status, url, fn, num, total):
+    print status, fn, num, total
 
 def main(articles_fn, out_dir):
+    bot = fetcher.Fetcher(report_cb=report_cb)
     articles = open(articles_fn).readlines()
     for i,name in enumerate(articles):
         name = name.strip().decode('utf8')
-        fn = os.path.join(out_dir, safe_fn(name))
-#        print i, len(articles), name, fn
-
+        fn = os.path.join(out_dir, layout.safe_fn(name))
         if os.path.exists(fn):
- #           print 'already there', fn
             continue
-        try:
-            print 'fetching', i, name
-            html = fetch(name)
-        except (urllib2.HTTPError,KeyError, urllib2.URLError) , e:
-            print i, repr(name), name, e
-            continue
-        open(fn, 'w').write(html)
-        time.sleep(fetch_delay)
-
+        url = 'http://en.m.wikipedia.org/wiki/' + urllib2.quote(name.encode('utf8'), safe='')
+        bot.add(url, fn)
+    bot.run()
 
 if __name__=='__main__':
     main(articles_fn=sys.argv[1], out_dir=sys.argv[2])
