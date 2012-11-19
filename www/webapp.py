6d7fe69f7aa59520fd278c42f98f56224b83ab40
Heiko Heiko <heiko@heiko.org>
2012-11-19 09:28:51 +0100

    added webapp

--- www/webapp.py
+++ www/webapp.py
@@ -2,28 +2,46 @@
 __author__ = 'heiko'
 import os
 import flask
+from whoosh.index import open_dir
+from whoosh.qparser import QueryParser
+
 app = flask.Flask(__name__)
-pwd = os.path.dirname(__file__)
-articles_dir = os.path.join(pwd, 'articles')
+articles_dir = os.path.join(app.root_path, 'wiki')
+search_index = open_dir(os.path.join(app.root_path, 'index'))
 
 
 @app.route('/')
 def index():
-    return 'Hello Homepage'
-
-
-@app.route('/search/')
-def search():
-    query = flask.request.form['query']
-    return 'Searched for %s' % query
+    return flask.render_template('index.html', num_articles = search_index.doc_count())
 
 @app.route('/wiki/<path:name>')
 def page(name):
+    if '..' in name or name.startswith('/'):
+        flask.abort(404)
     try:
-        assert not '..' in name
         return open(os.path.join(articles_dir, name)).read()
     except IOError:
         flask.abort(404)
+
+@app.route('/favicon.ico')
+def favicon():
+    return flask.send_from_directory(os.path.join(app.root_path, 'static'),
+        'favicon.png', mimetype='image/png')
+
+
+@app.route('/search')
+def search():
+    query_string = flask.request.args.get('search','')
+    with search_index.searcher() as searcher:
+        query = QueryParser("content", search_index.schema).parse(query_string)
+        results = searcher.search(query)
+        hits = [dict(title=r['title'], path=r['path']) for r in results]
+
+    # do we have a perfect match?
+    if hits and hits[0]['title'].lower() == query_string.lower():
+        return flask.redirect('wiki/' + hits[0]['title'])
+
+    return flask.render_template('search.html', query = query_string, results = hits)
 
 
 if __name__ == '__main__':
